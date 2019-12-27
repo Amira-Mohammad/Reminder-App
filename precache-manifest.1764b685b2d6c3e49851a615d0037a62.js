d81cb6d69ca338c2bed474429878372913fa516f
amira_1991 <geomiro2011@gmail.com>
2019-12-27 20:22:39 +0200

    Updates

---
+++ precache-manifest.1764b685b2d6c3e49851a615d0037a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1b8857c4acee326fbbc2f9675f4a45",
+    "url": "/Reminder-App/index.html"
+  },
+  {
+    "revision": "32aa36071ff963a1fabd",
+    "url": "/Reminder-App/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e0187a9b7b0bfa74fd4a",
+    "url": "/Reminder-App/static/css/main.6eee15a7.chunk.css"
+  },
+  {
+    "revision": "32aa36071ff963a1fabd",
+    "url": "/Reminder-App/static/js/2.aced2baa.chunk.js"
+  },
+  {
+    "revision": "e0187a9b7b0bfa74fd4a",
+    "url": "/Reminder-App/static/js/main.68d099c7.chunk.js"
+  },
+  {
+    "revision": "63d552e1fd042e1a3529",
+    "url": "/Reminder-App/static/js/runtime~main.6df9ffe7.js"
+  }
+]);
